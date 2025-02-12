6970c454d7623525c3d217cf909cefd7520edce7
2noScript <thangmk7@gmail.com>
2025-02-13 05:09:12 +0700

    fix: process root path

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,10 @@
 import fs from "fs";
 import path from "path";
-import os from "os";
 
 
 
 export function rootDirectory() {
-  let currentDir = path.resolve(__dirname);
+  let currentDir = process.cwd();
   while (!fs.existsSync(path.join(currentDir, "package.json"))) {
     const parentDir = path.dirname(currentDir);
     if (currentDir === parentDir) break;
@@ -13,35 +12,6 @@ export function rootDirectory() {
   }
   return currentDir;
 }
-
-
-
-export function checkService() {
-  const cpuInfo = os.cpus();
-  const totalMemoryGB = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2);
-  const freeMemoryGB = (os.freemem() / 1024 / 1024 / 1024).toFixed(2);
-
-  console.log("CPU Model:", cpuInfo[0].model);
-  console.log("CPU Cores:", cpuInfo.length);
-  console.log("Total Memory:", totalMemoryGB, "GB");
-  console.log("Free Memory:", freeMemoryGB, "GB");
-
-  const minCpuCores = 2;  // Tối thiểu 2 lõi CPU
-  const minMemoryGB = 2;  // Tối thiểu 2GB bộ nhớ
-
-  if (cpuInfo.length < minCpuCores) {
-      console.log("Warning: Not enough CPU cores. Minimum required is 2.");
-  } else {
-      console.log("CPU cores are sufficient.");
-  }
-
-  if (parseFloat(freeMemoryGB) < minMemoryGB) {
-      console.log("Warning: Not enough free memory. Minimum required is 2GB.");
-  } else {
-      console.log("Free memory is sufficient.");
-  }
-}
-
 
 
 
